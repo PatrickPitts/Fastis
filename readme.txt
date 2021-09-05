4c6115a6b6bc9c4daa36b68a26d27d3f669d3ba2
John Patrick Pitts <patrick.pittsj@gmail.com>
2021-09-05 16:25:44 -0700

    Commiting from IntelliJ directly

---
+++ readme.txt
@@ -0,0 +1,3 @@
+I just added this .txt file, and now I'm trying to control Commit/Push from my phone
+
+For whatever reason, it doesn't seem like the text is commiting properly??
--- src/model/NewDay.java
+++ src/model/NewDay.java
@@ -1,2 +1,9 @@
-package model;public class NewDay {
+package model;
+
+public class NewDay extends ICalendarEvent {
+
+    @Override
+    public String toString() {
+        return "NewDay{}";
+    }
 }
